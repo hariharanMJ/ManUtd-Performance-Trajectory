40d47bbb3fb163b75c4b98c66ecabcab465d3752
hariharanMJ <hariharan1713@gmail.com>
2026-09-12 20:46:14 +0530

    Day 3: add count_by_field, average_position, top_tier_seasons functions

--- season_dicts.py
+++ season_dicts.py
@@ -1,13 +1,13 @@
 
 season_data = [
-    {"season":"2010-11", "position":"1", "Manager":"Ferguson"},
-    {"season":"2011-12", "position":"2", "Manager":"Ferguson"},
-    {"season":"2012-13", "position":"1", "Manager":"Ferguson"},
-    {"season":"2013-14", "position":"7", "Manager":"Moyes"},
-    {"season":"2014-15", "position":"4", "Manager":"Van Gaal"},
-    {"season":"2015-16", "position":"5", "Manager":"Van Gaal"},
-    {"season":"2016-17", "position":"6", "Manager":"Mourinho"},
-    {"season":"2017-18", "position":"2", "Manager":"Mourinho"}
+    {"season":"2010-11", "position":1, "Manager":"Ferguson"},
+    {"season":"2011-12", "position":2, "Manager":"Ferguson"},
+    {"season":"2012-13", "position":1, "Manager":"Ferguson"},
+    {"season":"2013-14", "position":7, "Manager":"Moyes"},
+    {"season":"2014-15", "position":4, "Manager":"Van Gaal"},
+    {"season":"2015-16", "position":5, "Manager":"Van Gaal"},
+    {"season":"2016-17", "position":6, "Manager":"Mourinho"},
+    {"season":"2017-18", "position":2, "Manager":"Mourinho"}
 ]
 
 top4_finish = 0
@@ -15,7 +15,7 @@ outside_top4 = 0
 
 
 for row in season_data:
-    if row["position"] <= "4":
+    if row["position"] <= 4:
         top4_finish += 1
         print(row["season"],":", row["position"])
 
@@ -30,13 +30,52 @@ def count_by_managers(season_data):
     for row in season_data:
         manager = row["Manager"]
         manager_counts[manager] = manager_counts.get(manager, 0) + 1
-        '''if manager in manager_counts:
-            manager_counts[manager] = manager_counts[manager] + 1
-        else:
-            manager_counts[manager] = 1'''
     return manager_counts
     
 results = count_by_managers(season_data)
 for manager, count in results.items():
         label = "season" if count == 1 else "seasons"
         print(f"{manager} : {count} {label}")
+
+#Refactor count_by_managers function.
+def count_by_field(season_data, field_name):
+    field_counts = {}
+    for row in season_data:
+          field = row[field_name]
+          field_counts[field] = field_counts.get(field, 0) + 1
+
+    return field_counts
+
+def print_counts(results, label):
+     for key, count in results.items():
+         unit = "season" if count == 1 else "seasons"
+         print(f"{label} {key} : {count} {unit}")
+
+
+manager_results = count_by_field(season_data, "Manager")
+print_counts(manager_results, "Manager")
+
+position_results = count_by_field(season_data, "position")
+print_counts(position_results, "position")
+
+#Average Position Calculation
+def average_position(season_data):
+    season_counter = 0
+    position_counter = 0
+    for row in season_data:
+         position_counter = position_counter + row["position"]
+         season_counter = season_counter + 1 
+    avg = position_counter / season_counter
+    return avg
+print(average_position(season_data))
+
+#top tier seasons
+def top_tier_seasons(season_data, threshold = 4):
+    top_seasons = []
+    for row in season_data:
+        if row["position"] <= threshold:
+            top_seasons.append(row["season"])
+    return top_seasons     
+     
+print(top_tier_seasons(season_data))
+print(top_tier_seasons(season_data,6))
